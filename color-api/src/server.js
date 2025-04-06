fa62affb5b23a40fcc15b16736874e0b328a73cf
Jim124 <fengpingan065@gmail.com>
2025-04-06 10:14:58 +0100

    learn services

--- color-api/src/server.js
+++ color-api/src/server.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import os from 'os';
 
 const app = express();
 
 const port = 80;
 
+const color = 'blue';
+const hostName = os.hostname();
 app.get('/', (req, res) => {
-  res.send(`<h1 style="color:blue">hello from kubenetes</h1>`);
+  res.send(`<h1 style="color:${color}">hello from kubernetes</h1>`);
 });
 
+app.get('/api', (req, res) => {
+  const { format } = req.query;
+  if (format === 'json') {
+    return res.json({ color, hostName });
+  }
+  res.send(`{color:${color},hostname:${hostName}}`);
+});
 app.listen(port, () => {
   console.log(`serving is running on port ${port}`);
 });
