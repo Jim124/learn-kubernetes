5e15aef933d8670c25ec6d7f143e04d1243de352
Jim124 <fengpingan065@gmail.com>
2025-04-16 23:17:26 +0100

    learn starup probe

--- color-api/src/server.js
+++ color-api/src/server.js
@@ -7,6 +7,14 @@ const port = 80;
 
 const color = 'blue';
 const hostName = os.hostname();
+const delay_startup = process.env.DELAY_STARTUP === 'true';
+const fail_liveness = process.env.FAIL_LIVENESS === 'true';
+const fail_readiness =
+  process.env.FAIL_READINESS === 'true' ? Math.random < 0.5 : false;
+
+console.log(
+  `Delay startup:${delay_startup}, fail_liveness: ${fail_liveness}, fail_readiness: ${fail_readiness}`
+);
 app.get('/', (req, res) => {
   res.send(`<h1 style="color:${color}">hello from kubernetes</h1>`);
 });
@@ -18,6 +26,25 @@ app.get('/api', (req, res) => {
   }
   res.send(`{color:${color},hostname:${hostName}}`);
 });
+
+app.get('/ready', (req, res) => {
+  if (fail_readiness) {
+    return res.status(503).send('Service is not available to serve');
+  }
+  return res.status(200).send('ok');
+});
+app.get('/health', (req, res) => {
+  if (fail_liveness) {
+    return res.status(503).send('Service Unhealthy');
+  }
+  return res.status(200).send('ok');
+});
+if (delay_startup) {
+  const start = Date.now();
+  // purposefully block event loop and execution for 60 seconds.
+  // to illustrate startup probes
+  while (Date.now() - start < 60000) {}
+}
 app.listen(port, () => {
   console.log(`serving is running on port ${port}`);
 });
